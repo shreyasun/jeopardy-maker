e6506b77b55b0a93c2636e236312a59a899d23e4
shreyasun <shreyasundar27@gmail.com>
2023-08-27 14:08:47 -0700

    updates to routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./components/home/Home"
 import Board from "./components/board/Board"
-import CardsDeck from "./components/my-decks/CardsDeck"
+import CardsDeck from "./components/cards/CardsDeck"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Board />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Board" element={<Board />} />
           <Route path="/CardsDeck" element={<CardsDeck />} />
         </Routes>
       </div>
