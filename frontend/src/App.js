8a404cc5134c10db32b4c2d8256a2a156b71ff58
shreyasun <shreyasundar27@gmail.com>
2023-08-29 13:54:18 -0700

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
+import { React, useEffect, useState } from "react"
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./components/home/Home"
 import Board from "./components/board/Board"
 import CardsDeck from "./components/cards/CardsDeck"
+import Login from "./components/login/Login"
+import { AuthContextProvider } from "./context/AuthContext";
+import Protected from "./components/login/Protected";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Board" element={<Board />} />
-          <Route path="/CardsDeck" element={<CardsDeck />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <AuthContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Protected><Home /></Protected>} />
+            <Route path="/Board" element={<Protected><Board /></Protected>} />
+            <Route path="/CardsDeck" element={<Protected><CardsDeck /></Protected>} />
+          </Routes>
+        </Router>
+      </AuthContextProvider>
+    </div>
   );
 }
 export default App;
